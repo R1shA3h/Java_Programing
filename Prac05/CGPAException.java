2e50f49b2fe46403eba7d9a8e122fa2ec0f80e35
Rishabh <99585764+R1shA3h@users.noreply.github.com>
2023-05-19 15:52:24 +0530

    Delete Prac05 directory

--- Prac05/CGPAException.java
+++
@@ -1,9 +0,0 @@
-public class CGPAException extends Exception{
-    float f;
-    CGPAException(float cgpa){
-        f = cgpa;
-    }
-    public String toString(){
-        return "Invalid CGPA"+f+"\n";
-    }
-}
